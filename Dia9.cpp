a6eca378cb2e8858730795141c945b076fa94fac
Sol <sollopez0002@hotmail.com>
2024-06-21 19:43:24 -0400

    Day9

---
+++ Dia9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int suma(int a, int b) {
+	return (a+b);
+}
+
+int main(){
+    int num1, num2, resultado;
+    cout << "Ingrese un numero: " ;
+    cin >> num1;
+
+    cout << "Ingrese otro numero: " ;
+    cin >> num2;
+
+    resultado = suma(num1,num2);
+    cout << "La suma es: " << resultado  << endl;
+
+    return 0;
+
+}
